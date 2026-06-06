b4f3c31ed55f1da1a040ece7aeedee6f9128ecd2
Sachin <sachinrevankar10@gmail.com>
2026-06-07 00:02:26 +0530

    new commit

--- run_test.bat
+++ run_test.bat
@@ -4,6 +4,6 @@ cd /d "E:\Python Automation\nop_eCommerce Automation"
 
 call .venv\Scripts\activate
 
-pytest -v -s -m "regression" --html=Reports\reports.html testCases/ --browser chrome
+pytest -v -s --html=Reports\reports.html testCases/test_deleteCustomer.py --browser chrome
 
 pause
